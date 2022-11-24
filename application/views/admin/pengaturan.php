5855f3f1e1ccdebbfff6c1a4ebcd88e250c0a9fa
alfianrht <alvianrht@gmail.com>
2022-11-24 21:28:44 +0700

    Add rek form

--- application/views/admin/pengaturan.php
+++ application/views/admin/pengaturan.php
@@ -44,6 +44,35 @@
 								<input type="text" name="title" class="form-control" placeholder="Youtube" required="">
 							</div>
 						</div>
+						<hr class="my-2 border">
+						<h6>DATA PEMBAYARAN</h6>
+						<div class="col-lg-3">
+							<div class="form-group">
+								<label class="form-control-label" for="input-nama">Nomor Rekening <span class="text-danger">*</span></label>
+								<input type="text" name="title" class="form-control" placeholder="Nomor Rekening" required="">
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="form-group">
+								<label class="form-control-label" for="input-nama">Atas Nama <span class="text-danger">*</span></label>
+								<input type="text" name="title" class="form-control" placeholder="Nama pemilik" required="">
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="form-group">
+								<label class="form-control-label" for="input-nama">Nama Bank <span class="text-danger">*</span></label>
+								<input type="text" name="title" class="form-control" placeholder="Nama bank yang digunakan" required="">
+							</div>
+						</div>
+						<div class="col-lg-3">
+							<div class="form-group">
+								<label class="form-control-label" for="input-gambar">Logo Bank
+								<span class="text-danger">*</span></label>
+								<div class="mb-3">
+									<input class="form-control" type="file" name="file" id="image-source" onchange="previewImage()">
+								</div>
+							</div>
+						</div>
 						<div class="col-12 mt-3 d-grid">
 							<button type="submit" class="btn bg-gradient-dark mb-0">SIMPAN</button>
 							<button type="button" class="btn btn-link mb-0 text-secondary mt-2">KEMBALI</button>
