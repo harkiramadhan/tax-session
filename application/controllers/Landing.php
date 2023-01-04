13adad42a7db034294dcc7f81773dc1c3e914bf2
harkiramadhan <harkiramadhan@gmail.com>
2023-01-04 07:35:10 +0700

    updates

--- application/controllers/Landing.php
+++ application/controllers/Landing.php
@@ -1,15 +1,27 @@
 <?php
 class Landing extends CI_Controller{
-    // function __construct(){
-	// 	parent::__construct();
+    function __construct(){
+		parent::__construct();
 
-	// 	if($this->session->userdata('masuk') != TRUE)
-	// 		redirect('admin','refresh');
-	// }
+		$this->load->model([
+			'M_Banners',
+			'M_Partner',
+			'M_Benefit',
+			'M_Faq',
+			'M_Testimoni'
+		]);
+	}
     
     function index(){
 
-        $var['title'] = "Selamat Datang di Platform Kami";
+        $var = [
+			'title' => 'Selamat Datang di Platform Kami',
+			'banners' => $this->M_Banners->getActive(),
+			'partners' => $this->M_Partner->getActive(),
+			'benefits' => $this->M_Benefit->getActive(),
+			'faqs' => $this->M_Faq->getActive(),
+			'testimoni' => $this->M_Testimoni->getActive()
+		];
 
 		$this->load->view('layout/guest/header', $var);
 		$this->load->view('guest/landing', $var);
--- application/views/guest/landing.php
+++ application/views/guest/landing.php
@@ -26,111 +26,65 @@
 
         <!-- PATNER LOGO -->
         <section id="partner" class="container py-5">
-
             <div class="list-partner d-flex justify-content-around align-items-center">
-                <div>
-                    <img src="<?= base_url('assets/img/partner/tesla.png') ?>" alt="">
-                </div>    
-                <div>
-
-                    <img src="<?= base_url('assets/img/partner/amazon.png') ?>" alt="">
-                </div>
-                <div>
-
-                    <img src="<?= base_url('assets/img/partner/google.png') ?>" alt="">
-                </div>
-                <div>
-
-                    <img src="<?= base_url('assets/img/partner/facebook.png') ?>" alt="">
-                </div>
-                <div>
-
-                    <img src="<?= base_url('assets/img/partner/microsoft.png') ?>" alt="">
-                </div>
+                <?php foreach($partners->result() as $pr){ ?>
+                    <div>
+                        <img src="<?= base_url('uploads/partner/' . $pr->img) ?>" alt="">
+                    </div>    
+                <?php } ?>
             </div>
-        
         </section>
-
         <!-- PATNER LOGO END -->
 
         <!-- BANNER -->
-
         <section id="banner">
-
             <div class="container list-banner">
-
-                <div class="banner-item">
-                    <img src="<?= base_url('assets/img/guest/img-banner.png') ?>" alt="">
-                </div>
-
-                <div class="banner-item">
-                    <img src="<?= base_url('assets/img/guest/img-banner.png') ?>" alt="">
-                </div>
-
+                <?php foreach($banners->result() as $rb){ ?>
+                    <div class="banner-item">
+                        <img src="<?= base_url('uploads/banners/' . $rb->img) ?>" alt="">
+                    </div>
+                <?php } ?>
             </div>
-
         </section>
-
         <!-- BANNER END -->
 
         <!-- BENEFIT PELATIHAN -->
 
         <section id="benefit" class="py-5">
-            
             <div class="container">
-
                 <div class="head-section py-5">
                     <p class="text-uppercase mb-0">We provide tons of pathskill</p>
                     <h1 class="ms--2">Benefit <span>Pelatihan</span></h1>
                 </div>
 
                 <div class="row">
-                    <div class="col-lg-4">
-                        <div class="card-benefit card border-0 p-3">
-                            <div class="card-body">
-                                <img class="mb-5" src="<?= base_url('assets/img/guest/img-banner.png') ?>" alt="">
-                                <h4 class="mb-3"><span>The New Way</span> to Achieve Good Skills</h4>
-                                <p class="mb-0">We provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus on</p>
-                            </div>
-                        </div>    
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card-benefit card border-0 p-3" style="background: #751119;">
-                            <div class="card-body">
-                                <img class="mb-5 text-white" src="<?= base_url('assets/img/guest/img-banner.png') ?>" alt="">
-                                <h4 class="mb-3 text-white">The New Way to Achieve Good Skills</h4>
-                                <p class="mb-0 text-white">We provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus on</p>
-                            </div>
-                        </div>    
-                    </div>
-                    <div class="col-lg-4">
-                        <div class="card-benefit card border-0 p-3">
-                            <div class="card-body">
-                                <img class="mb-5" src="<?= base_url('assets/img/guest/img-banner.png') ?>" alt="">
-                                <h4 class="mb-3">The New Way to Achieve Good Skills</h4>
-                                <p class="mb-0">We provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus on</p>
-                            </div>
-                        </div>    
-                    </div>
+                    <?php 
+                        $no = 1;
+                        foreach($benefits->result() as $bnr){ 
+                    ?>
+                        <div class="col-lg-4">
+                            <div class="card-benefit card border-0 p-3" <?= ($no == 2) ? 'style="background: #751119;"' : '' ?>>
+                                <div class="card-body">
+                                    <img class="mb-5 <?= ($no == 2) ? 'text-white' : '' ?>" src="<?= base_url('assets/img/guest/img-banner.png') ?>" alt="">
+                                    <h4 class="mb-3 <?= ($no == 2) ? 'text-white' : '' ?>">The New Way to Achieve Good Skills</h4>
+                                    <p class="mb-0 <?= ($no == 2) ? 'text-white' : '' ?>">We provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus on</p>
+                                </div>
+                            </div>    
+                        </div>
+                    <?php $no++; } ?>
                 </div>
-
             </div>
-
         </section>
-
         <!-- BENEFIT PELATIHAN DONE -->
 
         <!-- STATISTIK -->
-        
         <section id="pelatihan">
-
             <div class="container py-5">
                 <div class="row">
                     <div class="col-lg-6 d-flex justify-content-center">
                         <img src="<?= base_url('/assets/img/guest/img-header-main.svg') ?>" alt="" class="w-75">
                     </div>
                     <div class="col-lg-6">
-
                         <div class="head-section">
                             <p class="text-uppercase mb-0">We provide tons of pathskill</p>
                             <h1 class="ms--2">Statistik<span> Pelatihan</span></h1>
@@ -163,19 +117,13 @@
                     </div>
                 </div>
             </div>
-
         </section>
-
         <!-- STATISTIK DONE -->
 
         <!-- DAFTAR PELATIHAN -->
-
         <section id="daftar-pelatihan" style="background: #FBF3F2;">
-
             <div class="container  py-5">
-                
                 <div class="row">
-
                     <div class="col-lg-7 pb-3">
                         <div class="head-section">
                             <p class="text-uppercase mb-0">We provide tons of pathskill</p>
@@ -205,112 +153,63 @@
                     </div>
                 </div>
             </div>
-
         </section>
-
         <!-- DAFTAR PELATIHAN DONE -->
 
         <!-- FAQ -->
-
         <section id="daftar-faq">
-
             <div class="container py-5">
-                
-
                 <div class="head-section">
                     <p class="text-uppercase mb-0">We provide tons of pathskill</p>
                     <h1 class="ms--2"><span>FAQ</span></h1>
                 </div>
 
                 <div class="accordion" id="accordionPanelsStayOpenExample">
-                    <div class="mb-3">
-                        <h2 class="accordion-header  faq-item" id="panelsStayOpen-headingOne">
-                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true" aria-controls="panelsStayOpen-collapseOne">
-                                Accordion Item #1
-                            </button>
-                        </h2>
-                        <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show" aria-labelledby="panelsStayOpen-headingOne">
-                            <div class="accordion-body">
-                                <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                    <?php 
+                        $nof = 1;
+                        foreach($faqs->result() as $fr){ 
+                    ?>
+                        <div class="mb-3">
+                            <h2 class="accordion-header faq-item" id="panelsStayOpen-headingOne<?= $fr->id ?>">
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseOne<?= $fr->id ?>" aria-expanded="<?= ($nof == 1) ? 'true' : 'false' ?>" aria-controls="panelsStayOpen-collapseOne"><?= $fr->question ?></button>
+                            </h2>
+                            <div id="panelsStayOpen-collapseOne<?= $fr->id ?>" class="accordion-collapse <?= ($nof == 1) ? 'show' : 'collapse' ?>" aria-labelledby="panelsStayOpen-headingOne<?= $fr->id ?>">
+                                <div class="accordion-body">
+                                    <?= $fr->answer ?>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="mb-3">
-                        <h2 class="accordion-header  faq-item" id="panelsStayOpen-headingTwo">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false" aria-controls="panelsStayOpen-collapseTwo">
-                            Accordion Item #2
-                        </button>
-                        </h2>
-                        <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingTwo">
-                            <div class="accordion-body">
-                                <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                            </div>
-                        </div>
-                    </div>
-                    <div class="mb-3">
-                        <h2 class="accordion-header  faq-item" id="panelsStayOpen-headingThree">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree" aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
-                            Accordion Item #3
-                        </button>
-                        </h2>
-                        <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse" aria-labelledby="panelsStayOpen-headingThree">
-                            <div class="accordion-body">
-                                <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
-                            </div>
-                        </div>
-                    </div>
+                    <?php $nof++; } ?>
                 </div>
             </div>
-
         </section>
-
         <!-- FAQ DONE -->
 
         <!-- TESTIMONI  -->
-
         <section id="testimoni">
-
             <div class="container py-5">
-                
                 <div class="head-section mb-4">
                     <p class="text-uppercase mb-0">We provide tons of pathskill</p>
                     <h1 class="ms--2">Testimoni <span>Peserta Kami</span></h1>
                 </div>
-
                 <div class="daftar-testimoni">
+                    <?php foreach($testimoni->result() as $tr){ ?>
                     <div class="testimoni-item card border-0 p-4">
                         <div class="row">
                             <div class="col-lg-3">
-                                <img class="mb-4" src="<?= base_url('assets/img/guest/img-banner.png') ?>" alt="">
-                                <h5 class="mb-1">Alfian Rahmatullah</h5>
-                                <p class="mb-0">Web Designer</p>
+                                <img class="mb-4" src="<?= base_url('uploads/testimoni/' . $tr->img) ?>" alt="">
+                                <h5 class="mb-1"><?= $tr->nama ?></h5>
+                                <p class="mb-0"><?= $tr->jabatan ?></p>
                             </div>
                             <div class="col-lg-9 d-flex align-items-center">
-                                <p>We provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus onWe provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus onWe provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus on</p>
+                                <p><?= $tr->deskripsi ?></p>
                             </div>
                         </div>
                     </div>
-
-
-                    <div class="testimoni-item card border-0 p-4">
-                        <div class="row">
-                            <div class="col-lg-3">
-                                <img class="mb-4" src="<?= base_url('assets/img/guest/img-banner.png') ?>" alt="">
-                                <h5 class="mb-1">Alfian Rahmatullah</h5>
-                                <p class="mb-0">Web Designer</p>
-                            </div>
-                            <div class="col-lg-9 d-flex align-items-center">
-                                <p>We provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus onWe provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus onWe provide tons of pathskill that you can choose and focus on We provide tons of pathskill that you can choose and focus on</p>
-                            </div>
-                        </div>
-                    </div>
+                    <?php } ?>
                 </div>
-                
-
             </div>
-
         </section>
-
         <!-- TESTIMONI DONE -->
 
         <!-- PERTANYAAN -->
